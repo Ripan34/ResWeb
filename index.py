120033769ba9efa96160c12bda28722435d55cdc
Ripandeep Singh <ripandeep.singh@sjsu.edu>
2021-11-02 20:39:09 -0700

    uploading the file

--- index.py
+++ index.py
@@ -1,7 +1,23 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+UPLOAD_FOLDER = 'static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 @app.route("/")
-def hello_world(name=None):
+def landing(name=None):
     return render_template("landingPage.html", name=name);
+
+@app.route("/fill")
+def fill(name=None):
+    return render_template("fillResume.html", name=name);
+
+@app.route("/resume", methods=['POST'])
+def upload_res():
+    data = request.files['doc_file']
+    filename = secure_filename(data.filename)
+    data.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return render_template("fillResume.html", fileName=filename);
